df7c57e4ef4751a25c9490d49528c1b2d2b41358
zhikun.luo <zhikun.luo@yz-gpu026.hogpu.cc>
2019-06-11 10:03:41 +0800

    add softmax regression

---
+++ softmax_regression.py
@@ -0,0 +1,83 @@
+import d2lzh as d2l
+from mxnet import autograd, nd
+
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+num_inputs = 784
+num_outputs = 10
+
+W = nd.random.normal(scale=0.01, shape=(num_inputs, num_outputs))
+b = nd.zeros(num_outputs)
+
+W.attach_grad()
+b.attach_grad()
+
+def softmax(X):
+	X_exp = X.exp()
+	partition = X_exp.sum(axis=1, keepdims=True)
+	return X_exp / partition
+
+# define model
+def net(X):
+	return softmax(nd.dot(X.reshape((-1, num_inputs)), W) + b)
+
+# define loss function
+def cross_entropy(y_hat, y):
+	return -nd.pick(y_hat, y).log()
+
+# calculate accuracy
+def accuracy(y_hat, y):
+	return (y_hat.argmax(axis=1) == y.astype('float32')).mean()
+
+# evaluate accuracy
+def evaluate_accuracy(data_iter, net):
+	acc_sum, n = 0.0, 0
+	for X, y in data_iter:
+		y = y.astype('float32')
+		acc_sum += (net(X).argmax(axis=1) == y).sum().asscalar()
+		n += y.size
+	return acc_sum / n
+
+# train model
+num_epochs, lr = 5, 0.1
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params=None, lr=None, trainer=None):
+	for epoch in range(num_epochs):
+		train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+		for X, y in train_iter:
+			with autograd.record():
+				y_hat = net(X)
+				l = loss(y_hat, y).sum()
+			l.backward()
+			if trainer is None:
+				d2l.sgd(params, lr, batch_size)
+			else:
+				trainer.step(batch_size)
+			y = y.astype('float32')
+			train_l_sum += l.asscalar()
+			train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()
+			n += y.size
+		test_acc = evaluate_accuracy(test_iter, net)
+		print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))	
+
+
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, batch_size,
+          [W, b], lr)
+
+
+# show some results
+'''
+for X, y in test_iter:
+	break
+
+true_labels = d2l.get_fashion_mnist_labels(y.asnumpy())
+pred_labels = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1).asnumpy())
+titles = [true + '\n' + pred for true, pred in zip(true_labels, pred_labels)]
+d2l.show_fashion_mnist(X[0:9], titles[0:9])
+'''
+
+
+
+# 1. loss function 这么定义可能存在什么问题？
+# 2. softmax 运算这么定义可能存在什么问题？数值不稳定问题
